028e0ce98e71c279125e8267ada4be384f84ea2a
hussain-1913010 <hussain1913010@stud.kuet.ac.bd>
2024-11-15 19:50:48 +0600

    daisy dashboard integration
    Responsive sidebar, custom scss added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,85 @@
-// import './App.css'
+import { useState, useEffect } from "react";
+import { AiOutlineDashboard } from "react-icons/ai";
+import { IoIosCheckboxOutline } from "react-icons/io";
+import { HiOutlineUsers } from "react-icons/hi2";
+
+import "./App.scss";
 
 import Navbar from "./components/Navbar";
 
 function App() {
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date();
+      setDate(now.toLocaleDateString());
+      setTime(now.toLocaleTimeString());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
-      <div className="container mx-auto">
-        <Navbar />
-        <h1 className="text-3xl font-bold underline">This is the example of tailwind underline classes!</h1>
+      <div className="mx-auto">
+        <div className="drawer lg:drawer-open">
+          <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
+          <div className="drawer-content">
+            {/* Page content here */}
+            <Navbar />
+            <div className="gratings">
+              <div className="admin">
+                <h1>Welcome !</h1>
+                <h2>
+                  Muneem Hussain <span>(Admin)</span>
+                </h2>
+              </div>
+              <div className="date_time">
+                <h4>
+                  Today's Date: <span>{date}</span>
+                </h4>
+                <h3>
+                  Current Time: <span>{time}</span>
+                </h3>
+              </div>
+            </div>
+          </div>
+          <div className="drawer-side">
+            <label
+              htmlFor="my-drawer-4"
+              aria-label="close sidebar"
+              className="drawer-overlay"
+            ></label>
+            <div className="menu bg-base-200 min-h-full w-80 p-4">
+              {/* Sidebar content here */}
+              <div className="mx-auto mt-4">
+                <h1 className=" text-xl lg:hidden md:inline-block">
+                  Admin Panel
+                </h1>
+                <ul className="menu menu-lg bg-base-200 rounded-box w-56">
+                  <li>
+                    <a>
+                      <AiOutlineDashboard size={24} />
+                      Dashboard
+                    </a>
+                  </li>
+                  <li>
+                  <a>
+                      <IoIosCheckboxOutline  size={24} />
+                      Checklist
+                    </a>
+                  </li>
+                  <li>
+                  <a>
+                      <HiOutlineUsers  size={24} />
+                      Users
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,58 @@
+import { HiOutlineMenuAlt1 } from "react-icons/hi";
 
 const Navbar = () => {
   return (
     <div className="navbar bg-base-100">
-  <div className="flex-1">
-    <a className="btn btn-ghost text-xl">daisyUI</a>
-  </div>
-  <div className="flex-none">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Link</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="bg-base-100 rounded-t-none p-2">
-            <li><a>Link 1</a></li>
-            <li><a>Link 2</a></li>
+      <div className="navbar-start">
+        <label htmlFor="my-drawer-4" className="lg:hidden md:inline-block">
+          <HiOutlineMenuAlt1 size={24} />
+        </label>
+        <a className="btn btn-ghost text-xl">Admin Panel</a>
+      </div>
+      <div className="navbar-center">
+        <div className="form-control">
+          <input
+            type="text"
+            placeholder="Search"
+            className="input input-bordered w-24 md:w-auto"
+          />
+        </div>
+      </div>
+      <div className="navbar-end">
+        <div className="dropdown dropdown-end">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle avatar"
+          >
+            <div className="w-10 rounded-full">
+              <img
+                alt="Tailwind CSS Navbar component"
+                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+              />
+            </div>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-200 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <a className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
+            </li>
+            <li>
+              <a>Settings</a>
+            </li>
+            <li>
+              <a>Logout</a>
+            </li>
           </ul>
-        </details>
-      </li>
-    </ul>
-  </div>
-</div>
-  )
-}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
